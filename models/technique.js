05a7d7f503532b34117ead3a8a1d832a350d52ce
Lillian Chernin <l.smith1654@gmail.com>
2018-01-18 12:59:05 -0600

    add test dialog for join link

--- models/technique.js
+++ models/technique.js
@@ -3,7 +3,9 @@ const Schema = mongoose.Schema;
 
 const techniqueSchema = new Schema ({
   description: String,
-  comments: Array
+  comments: Array,
+  approved: {type: Boolean, default: false},
+  author: {type: String, default: "db_owner"}
 })
 
 const Technique = mongoose.model('Technique', techniqueSchema);
---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+  $('.header-signup').on('click', () => {
+    $('.signupDialog').dialog();
+  })
+})
