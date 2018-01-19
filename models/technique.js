ee29c48d920bc39f1b1d45442bb27e6351b09df0
Lillian Chernin <l.smith1654@gmail.com>
2018-01-18 20:34:16 -0600

    update footer

--- models/technique.js
+++ models/technique.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const Comment = require('./comment');
+const Comments = require('./comment');
 
 const techniqueSchema = new Schema ({
   shortDescription: String,
   detailedDescription: String,
-  comments: [Comment.Schema],
+  comments: [Comments.Schema],
   approved: {type: Boolean, default: false},
   author: {type: String, default: "db_owner"}
 })
