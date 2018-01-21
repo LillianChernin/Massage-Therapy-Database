a53cef24c9d7fa9a5d7a1c1d4c3ec5a427878d91
Lillian Chernin <l.smith1654@gmail.com>
2018-01-20 22:16:05 -0600

    wip add new technique to disorder

--- public/js/app.js
+++ public/js/app.js
@@ -7,4 +7,26 @@ $(document).ready(() => {
     $('.loginDialog').removeClass('hidden');
     $('.loginDialog').dialog();
   })
+  $('.submitNewTechniqueButton').on('click', (e) => {
+    console.log(e);
+    let shortDescription = e.target.previousElementSibling.previousElementSibling.value;
+    let detailedDescription = e.target.previousElementSibling.value;
+    let url = '/disorders/api/' + e.target.dataset.disorderId + '/techniques';
+    $.ajax({
+      method: "POST",
+      url: url,
+      data: {
+        shortDescription: shortDescription,
+        detailedDescription: detailedDescription
+      },
+      success: (json) => {
+        e.target.previousElementSibling.previousElementSibling.setAttribute('value', '');
+        e.target.previousElementSibling.setAttribute('value', '');
+        console.log('success');
+      },
+      error: () => {
+        console.log("ajax post error!");
+      }
+    })
+  })
 })
--- routes/disorders.js
+++ routes/disorders.js
@@ -19,7 +19,6 @@ disorderRoutes.get('/', (req, res) => {
 disorderRoutes.get('/:id', (req, res) => {
   let currentId = req.params.id;
   db.Disorder.findById(currentId, (err, disorder) => {
-    console.log(disorder);
     res.render('./disorders/individual-disorder', {
       documentTitle: disorder.name,
       data: disorder
@@ -39,6 +38,37 @@ disorderRoutes.get('/:category', (req, res) => {
 disorderRoutes.get('/:id/techniques', (req, res) => {
   let currentId = req.params.id;
   db.Disorder.findById(currentId, (err, disorder) => {
+    res.render('./disorders/techniques-by-disorder-id', {
+      documentTitle: disorder.name + ' - Techniques',
+      data: disorder
+    })
+  })
+})
+
+disorderRoutes.get('/:id/techniques/:technique_id', (req, res) => {
+  let currentTechnique;
+  db.Disorder.findById(req.params.id, (err, disorder) => {
+    for (let i = 0; i < disorder.techniques.length; i++) {
+      if (disorder.techniques[i]._id == req.params.technique_id) {
+        currentTechnique = disorder.techniques[i];
+      }
+    }
+    res.render('./techniques/individual-technique', {
+      documentTitle: disorder.name + " - " + currentTechnique.shortDescription,
+      data: currentTechnique,
+      disorderId: req.params.id
+    })
+  })
+})
+
+disorderRoutes.post('/api/:id/techniques', (req, res) => {
+  let newTechnique = new db.Technique(req.body);
+  db.Disorder.findByIdAndUpdate(req.params.id,
+  {$push: {techniques: req.body}},
+  {safe: true, upsert: true, new: true}, (err, disorder) => {
+    if (err) {
+      res.status(500).send(err);
+    }
     res.render('./disorders/techniques-by-disorder-id', {
       documentTitle: disorder.name + ' - Techniques',
       data: disorder
